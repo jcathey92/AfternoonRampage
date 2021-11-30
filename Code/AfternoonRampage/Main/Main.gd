9b3b125e67f61830cb45a0e2c0b983624369f4fc
Kyle <47279470+unblinky@users.noreply.github.com>
2021-11-29 23:20:58 -0800

    Setup project folders and classes. Added TODOManager addon.
    Classes:
    - Main (singleton)
    - Player
    - Gift
    - DeliveryPoint
    - Obstacle

---
+++ Code/AfternoonRampage/Main/Main.gd
@@ -0,0 +1,27 @@
+#==============================================================================
+# AFTERNOON RAMPAGE
+# Genre: Romantic Novel + Pixel Sprite Beat 'em up!
+# Examples: Streets of Rage, Double Dragon, RCR, Castle Crashers.
+# Input: 2D GamePad + Keyboard (Twin Stick?)
+#
+# DESIGN
+# - Time management / limit. Timers and Progress bars for the UI.
+# - Avoid obsticals 
+# - Player is given a certain amount of time to collect the item and return it to
+#   the delivery point.
+#
+# MAIN CLASSES
+# - Player
+# - Gift
+# - DeliveryPoint
+# - Obstacle
+#==============================================================================
+
+extends Node
+
+const PLAYER_PS: PackedScene = preload("res://Player/Player.tscn")
+const GIFT_PS: PackedScene = preload("res://Gift/Gift.tscn")
+
+
+func TestPrint(message: String):
+	print(message)
---
+++ Code/AfternoonRampage/Player/Player.gd
@@ -0,0 +1,5 @@
+extends Node2D
+class_name Player
+
+func _ready():
+	Main.TestPrint("Hello Main!")
