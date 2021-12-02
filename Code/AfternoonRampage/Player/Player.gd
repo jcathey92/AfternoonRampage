ea570fc3731ec7d4ee76532ac9656a3944d2c1dd
Extrevorted <extrevorted@gmail.com>
2021-12-01 21:27:37 -0800

    Add Bounding Box

--- Code/AfternoonRampage/Player/Player.gd
+++ Code/AfternoonRampage/Player/Player.gd
@@ -1,5 +1,7 @@
 #Node
 extends KinematicBody2D
+#Class
+class_name Player
 #Variables
 var input_vector: Vector2 = Vector2.ZERO # Input Recivers 
 var     velocity: Vector2 = Vector2.ZERO # Movement Calcuation
@@ -9,5 +11,4 @@ func _physics_process(delta):
 	# Getting Input
 	input_vector = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"), Input.get_action_strength("down") - Input.get_action_strength("up")).normalized()
 	# Movement
-#	velocity = move_and_slide(velocity.move_toward(input_vector * speed, speed * 1.5 * delta))
-	velocity = move_and_slide(input_vector * speed)
+	velocity = move_and_slide(velocity.move_toward(input_vector * speed, speed * 1.8 * delta))
