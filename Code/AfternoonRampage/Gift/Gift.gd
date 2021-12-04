aae947f04438590547dfa7e0b58a292a0075807b
Extrevorted <extrevorted@gmail.com>
2021-12-03 23:25:26 -0800

    Added Player Inventory, Gift Pick Up logic, Bounding Box

--- Code/AfternoonRampage/Gift/Gift.gd
+++ Code/AfternoonRampage/Gift/Gift.gd
@@ -1,5 +1,20 @@
-extends Node2D
+extends Area2D
 class_name Gift
 
-# enum gift types?
+var gift_type
 
+enum{
+	Red,
+	Blue
+}
+
+func _ready():
+	if round(rand_range(0,1)) == 0:
+		gift_type = Red
+	else:
+		gift_type = Blue
+
+func _on_Gift_body_entered(body):
+	if body is Player:
+		body.pick_up_gift(gift_type)
+		queue_free()
--- Code/AfternoonRampage/Player/Player.gd
+++ Code/AfternoonRampage/Player/Player.gd
@@ -1,13 +1,21 @@
 #Node
 extends KinematicBody2D
+class_name Player
 #Variables
 var input_vector: Vector2 = Vector2.ZERO # Input Recivers 
 var     velocity: Vector2 = Vector2.ZERO # Movement Calcuation
 var        speed:   float = 400          # Top speed for player movement
+var acceleration:   float = 4
+var    Inventory:   Array = []
 #Functions
 func _physics_process(delta):
-	# Getting Input
+# Getting Input
 	input_vector = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"), Input.get_action_strength("down") - Input.get_action_strength("up")).normalized()
-	# Movement
-#	velocity = move_and_slide(velocity.move_toward(input_vector * speed, speed * 1.5 * delta))
-	velocity = move_and_slide(input_vector * speed)
+# Movement
+	velocity = move_and_slide(velocity.move_toward(input_vector * speed, speed * acceleration * delta))
+#Debug
+	if Input.is_action_just_pressed("ui_accept"):
+		print(Inventory)
+func pick_up_gift(gift_type):
+	if Inventory.size() <= 0:
+		Inventory.append(gift_type) 
