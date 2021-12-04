79ee2e6ed9a034dabe723562a6d0e418a17184d7
Extrevorted <extrevorted@gmail.com>
2021-12-03 23:55:12 -0800

    Fixed Changes

--- Code/AfternoonRampage/Player/Player.gd
+++ Code/AfternoonRampage/Player/Player.gd
@@ -6,13 +6,13 @@ var input_vector: Vector2 = Vector2.ZERO # Input Recivers
 var     velocity: Vector2 = Vector2.ZERO # Movement Calcuation
 var        speed:   float = 400          # Top speed for player movement
 var acceleration:   float = 4            # 
-var    Inventory:   Array = []
+var    Inventory:   Array = []           # 
 #Functions
 func _physics_process(delta):
-# Getting Input - The Vector2 is going to be es
+# Getting Input 
 	input_vector = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"), Input.get_action_strength("down") - Input.get_action_strength("up")).normalized()
 # Movement
-	velocity = move_and_slide(velocity.move_toward(input_vector * speed, speed * 1.8 * delta))
+	velocity = move_and_slide(velocity.move_toward(input_vector * speed, speed * acceleration * delta))
 #Debug
 	if Input.is_action_just_pressed("ui_accept"):
 		print(Inventory)
