8aaae6d24531f01ee1b5178577ec838c169be6fa
Extrevorted <extrevorted@gmail.com>
2021-12-08 18:03:34 -0800

    Added Partial Gift Pick Up Code

--- Code/AfternoonRampage/Gift/Gift.gd
+++ Code/AfternoonRampage/Gift/Gift.gd
@@ -3,3 +3,11 @@ class_name Gift
 
 # enum gift types?
 
+onready var sprite: AnimatedSprite = $Sprite
+
+func _ready():
+	sprite.frame = round(rand_range(0,3))
+
+func _on_Gift_body_entered(body):
+	if body is Player:
+		queue_free()
